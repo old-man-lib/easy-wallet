5ec02da00377d5b12ef6641c607b2af3c7658aef
Maxim <51321197+old-man-lib@users.noreply.github.com>
2023-02-12 23:03:58 +0300

    Some Updates
    Add language support for keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,29 +1,67 @@
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Menu Keyboard
-menu = ReplyKeyboardMarkup(resize_keyboard=True)
-menu.row(
-	KeyboardButton('Получить📥'),
-	KeyboardButton('Профиль👤'),
-	KeyboardButton('Отправить📤')
-)
-menu.row(
-	KeyboardButton('История транзакций🧾')
-)
-menu.row(
-	KeyboardButton('API🔧'),
-	KeyboardButton('Настройки⚙'),
-	KeyboardButton('Справка📑')
-)
+import config, json
 
-# Buy Keyboard
-accept_or_cancel = ReplyKeyboardMarkup(resize_keyboard=True)
-accept_or_cancel.row(
-	KeyboardButton('Подтвердить✔️'),
-	KeyboardButton('Отмена❌')
-)
+DEFAULT_LANGUAGE = config.default_language
 
-# Cencel Keyboard
-cancel = ReplyKeyboardMarkup(resize_keyboard=True).row(
-	KeyboardButton('Отмена❌')
-)
+json_file = './data/language.json'
+with open(json_file, 'r', encoding='utf-8') as file:
+	language_json = json.load(file)
+
+def GetTextByTagAndLanguage(language_json, language, tag) -> list:
+	if not(language in language_json):
+		language = DEFAULT_LANGUAGE
+
+	language_text = language_json[language][tag]
+
+	return language_text
+
+def GetLanguageList() -> list:
+	languages = []
+	for language in language_json:
+		languages.append(language)
+
+	return languages
+
+def GetMenuKeyboard(language):
+	menu_keyboard_text = GetTextByTagAndLanguage(language_json, language, 'menu_keyboard')
+
+	# Menu Keyboard
+	menu = ReplyKeyboardMarkup(resize_keyboard=True)
+	menu.row(
+		KeyboardButton(menu_keyboard_text['get']),
+		KeyboardButton(menu_keyboard_text['profile']),
+		KeyboardButton(menu_keyboard_text['send'])
+	)
+	menu.row(
+		KeyboardButton(menu_keyboard_text['transaction_history'])
+	)
+	menu.row(
+		KeyboardButton(menu_keyboard_text['api']),
+		KeyboardButton(menu_keyboard_text['settings']),
+		KeyboardButton(menu_keyboard_text['reference'])
+	)
+
+	return menu
+
+def GetBuyKeyboard(language):
+	buy_keyboard_text = GetTextByTagAndLanguage(language_json, language, 'buy_keyboard')
+	
+	# Buy Keyboard
+	accept_or_cancel = ReplyKeyboardMarkup(resize_keyboard=True)
+	accept_or_cancel.row(
+		KeyboardButton(buy_keyboard_text['accept']),
+		KeyboardButton(buy_keyboard_text['cancel'])
+	)
+
+	return accept_or_cancel
+
+def GetCancelKeyboard(language):
+	cancel_keyboard_text = GetTextByTagAndLanguage(language_json, language, 'cancel_keyboard')
+
+	# Cencel Keyboard
+	cancel = ReplyKeyboardMarkup(resize_keyboard=True).row(
+		KeyboardButton(cancel_keyboard_text['cancel'])
+	)
+
+	return cancel
---
+++ networks_imports.py
@@ -0,0 +1,7 @@
+from py_crypto_hd_wallet import HdWalletBip44Coins
+
+networks = {
+	"ethereum" : HdWalletBip44Coins.ETHEREUM,
+	"binance" : HdWalletBip44Coins.BINANCE_CHAIN,
+	"polygon" : HdWalletBip44Coins.POLYGON
+}
