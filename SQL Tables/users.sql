01f7137e8885c9fe9376023acd0d0870aa7124d9
Maxim <51321197+old-man-lib@users.noreply.github.com>
2023-02-12 23:07:32 +0300

    Nothing

--- SQL Tables/users.sql
+++ SQL Tables/users.sql
@@ -1,3 +1,4 @@
+/*Sqlite Table*/
 CREATE TABLE users (
     id integer PRIMARY KEY AUTOINCREMENT,
     tg_id int(11) DEFAULT NULL,
@@ -8,4 +9,18 @@ CREATE TABLE users (
     reg_datetime datetime DEFAULT NULL,
     last_use datetime DEFAULT NULL,
     status_mes text DEFAULT NULL
+)
+
+/*MySql Table*/
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    tg_id int(11) DEFAULT NULL,
+    tg_username text DEFAULT NULL,
+    wallet_mnemonic text DEFAULT NULL,
+    milling integer DEFAULT 1,
+    language text DEFAULT NULL,
+    reg_datetime datetime DEFAULT NULL,
+    last_use datetime DEFAULT NULL,
+    status_mes text DEFAULT NULL,
+    PRIMARY KEY (id)
 )
